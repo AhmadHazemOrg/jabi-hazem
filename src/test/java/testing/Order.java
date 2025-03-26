3c5c7fff33e1ffbce6b28fb87a31d60d4787300d
Hazem <mazen@192.168.1.2>
2025-03-26 14:29:40 +0300

    second feature

---
+++ src/test/java/testing/Order.java
@@ -0,0 +1,62 @@
+package testing;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Order {
+    @Given("a customer is logged into the system")
+    public void a_customer_is_logged_into_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the customer navigates to the order history section")
+    public void the_customer_navigates_to_the_order_history_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should display a list of past meal orders")
+    public void the_system_should_display_a_list_of_past_meal_orders() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a chef is logged into the system")
+    public void a_chef_is_logged_into_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a customer has past meal orders in the system")
+    public void a_customer_has_past_meal_orders_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the chef views the customer's profile")
+    public void the_chef_views_the_customer_s_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should display the customer's past meal orders")
+    public void the_system_should_display_the_customer_s_past_meal_orders() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the customer navigates to the personalized meal plans section")
+    public void the_customer_navigates_to_the_personalized_meal_plans_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should generate meal suggestions based on past orders")
+    public void the_system_should_generate_meal_suggestions_based_on_past_orders() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
