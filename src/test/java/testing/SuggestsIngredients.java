9d76b86463c70b258d888e457fce790eed8d9f08
Hazem <mazen@192.168.1.2>
2025-03-26 14:52:14 +0300

    fourth feature

---
+++ src/test/java/testing/SuggestsIngredients.java
@@ -0,0 +1,67 @@
+package testing;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SuggestsIngredients {
+    @Given("a customer is logged into the system")
+    public void a_customer_is_logged_into_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the customer has dietary restrictions stored in the system")
+    public void the_customer_has_dietary_restrictions_stored_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the customer selects a meal that contains restricted ingredients")
+    public void the_customer_selects_a_meal_that_contains_restricted_ingredients() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should suggest alternative ingredients that fit the customer's dietary restrictions")
+    public void the_system_should_suggest_alternative_ingredients_that_fit_the_customer_s_dietary_restrictions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the system has suggested alternative ingredients for a meal")
+    public void the_system_has_suggested_alternative_ingredients_for_a_meal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the customer accepts a suggested substitution")
+    public void the_customer_accepts_a_suggested_substitution() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should update the meal with the chosen alternative ingredient")
+    public void the_system_should_update_the_meal_with_the_chosen_alternative_ingredient() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a customer has placed an order with a substituted ingredient")
+    public void a_customer_has_placed_an_order_with_a_substituted_ingredient() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the chef accesses the order details")
+    public void the_chef_accesses_the_order_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should notify the chef of the ingredient substitution")
+    public void the_system_should_notify_the_chef_of_the_ingredient_substitution() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
